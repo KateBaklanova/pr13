3582242d49710638408d12d2f69b540e85477f93
KateBaklanova <120919792+KateBaklanova@users.noreply.github.com>
2022-12-18 18:20:36 +0300

    Add files via upload

---
+++ r13/src/Person.java
@@ -0,0 +1,34 @@
+public class Person {
+      String name;
+      String surname;
+      String ot;
+
+      Person(String surname, String name, String ot)
+      {
+          this.name = name;
+          this.surname = surname;
+          this.ot = ot;
+      }
+      Person(){};
+
+      String getFIO()
+      {
+          if(name.isEmpty() || surname.isEmpty() || ot.isEmpty())
+          {
+              System.out.println("никто");
+              return "";
+          }
+          else
+          {
+              return (surname+" "+name.charAt(0)+"."+ot.charAt(0));
+          }
+      }
+
+      public static void main(String[] args)
+      {
+          Person p1 = new Person("Бакланова", "Екатерина", "Сергеевна");
+          Person p2 = new Person("", "", "");
+          System.out.println(p1.getFIO());
+          System.out.println(p2.getFIO());
+      }
+}
